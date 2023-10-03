4c71393b5cfef60c632745ba56cbaddbe78ea844
Vitalii Prokopenko <doc8259@gmail.com>
2023-10-03 21:53:13 +0300

    updates

--- src/index.js
+++ src/index.js
@@ -1,2 +1,10 @@
 console.log('Hello world!');
-console.log('Hello everybody!')
+console.log('Hello everybody!');
+
+// Create heading node
+const heading = document.createElement('h1')
+heading.textContent = 'Hello world! Hello everybody!'
+
+// Append heading node to the DOM
+const app = document.querySelector('#root')
+app.append(heading)
