1ae7d29623a97e1f2fc73c9978d915e2669ffe9c
Vitalii Prokopenko <doc8259@gmail.com>
2023-10-12 22:02:39 +0300

    updates logos

--- src/index.js
+++ src/index.js
@@ -1,21 +1,27 @@
-import wallpaperImage from '@images/wallpaper.jpg';
+import logoImage from '@images/logo/logo.png';
+import webpackLogoImage from '@images/logo/webpack-logo.png';
 import '@scss/main.scss';
-import dataXml from '@data/data.xml';
-import dataCsv from '@data/data.csv';
 
-console.log('Hello world!');
-console.log('Hello everybody!');
-console.log(dataXml);
-console.log(dataCsv);
+// import dataXml from '@data/data.xml';
+// import dataCsv from '@data/data.csv';
+
+// console.log('Hello world!');
+// console.log('Hello everybody!');
+// console.log(dataXml);
+// console.log(dataCsv);
+
+// Create logo
+const logo = document.createElement('img');
+logo.src = logoImage;
 
 // Create heading node
 const heading = document.createElement('h1');
-heading.textContent = 'Hello world! Hello everybody!';
+heading.textContent = 'Webpack 5 Boilerplate';
 
-// Create image
-const wallpaper = document.createElement('img');
-wallpaper.src = wallpaperImage;
+// Create webpack logo
+const webpackLogo = document.createElement('img');
+webpackLogo.src = webpackLogoImage;
 
 // Append heading node to the DOM
 const app = document.querySelector('#root');
-app.append(heading, wallpaper);
+app.append(logo, heading, webpackLogo);
